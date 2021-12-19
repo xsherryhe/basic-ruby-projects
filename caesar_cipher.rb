878ebd8d75a69ed2909186704184da2c0af58720
Sherry He <xsherryhe@gmail.com>
2021-12-18 19:50:01 -0500

    Completed caesar_cipher.rb

---
+++ caesar_cipher.rb
@@ -0,0 +1,7 @@
+def caesar_cipher(string, shift)
+  string.gsub(/[A-Za-z]/) do |char| 
+    code = char.ord
+    offset = code < 97 ? 65 : 97
+    ((code - offset + shift) % 26 + offset).chr
+  end
+end
